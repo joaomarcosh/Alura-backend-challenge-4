bf204ab9d24f1a286c30886315cf3d78f6b97244
joaomarcosh <joaomarcosh@gmail.com>
2022-11-22 09:06:37 -0400

    Add server config file

---
+++ src/config/server.config.ts
@@ -0,0 +1,5 @@
+export default {
+  test: { host: '127.0.0.1' },
+  development: { host: '127.0.0.1' },
+  production: { host: '0.0.0.0' },
+}
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import fastifyCookie from '@fastify/cookie';
 import { UserService } from './user/user.service';
 import { Roles } from './user/enums/user-roles.enum';
+import ServerConfig from './config/server.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -21,9 +23,18 @@ async function bootstrap() {
     secret: 'my-secret',
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.setGlobalPrefix('api');
+  
+  const config = new DocumentBuilder()
+    .setTitle('Alura backend challenge 4')
+    .setDescription('Challenge API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port, ServerConfig[process.env.NODE_ENV]['host']);
   
   const userService = await app.get(UserService);
   const adminExists = await userService.findOneByUsername(process.env.ADMIN_USER);
